ea7c5d8c5bafa269f66b1dcd87694fb32a548ff6
Bùi Thanh Bách <btbach25@gmail.com>
2024-09-18 17:12:42 +0700

    add lots of thing

---
+++ lib/home_screen/trending/trending_widget.dart
@@ -0,0 +1,40 @@
+import 'package:fe_garbage_classification_app/home_screen/trending/mini_post_widget.dart';
+import 'package:flutter/material.dart';
+
+class trendingWid extends StatefulWidget {
+  const trendingWid({super.key});
+
+  @override
+  State<trendingWid> createState() => _trendingWidState();
+}
+
+class _trendingWidState extends State<trendingWid> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width - 20, 
+        //height: 500,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  
+                  Text("Trending !" ,  style: TextStyle( fontSize: 20 ,  color:Color.fromARGB(255, 79, 187, 90), fontWeight: FontWeight.bold , ),),
+                ],
+              )
+            ),
+            miniPostWidget(),
+            miniPostWidget(),
+            miniPostWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
