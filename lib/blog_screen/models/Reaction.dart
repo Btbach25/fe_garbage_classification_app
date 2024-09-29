d3fc9fe2ba59f6c983bf077827ba1aed353433e8
kan3103 <khang310304@gmail.com>
2024-09-29 13:47:08 +0700

    Fix API

--- lib/blog_screen/models/Reaction.dart
+++ lib/blog_screen/models/Reaction.dart
@@ -1,16 +1,26 @@
 class Reaction {
   int? id;
   String? authorName;
+  String? createdAt;
+  String? updatedAt;
   int? reactionType;
   int? author;
   int? content;
 
   Reaction(
-      {this.id, this.authorName, this.reactionType, this.author, this.content});
+      {this.id,
+      this.authorName,
+      this.createdAt,
+      this.updatedAt,
+      this.reactionType,
+      this.author,
+      this.content});
 
   Reaction.fromJson(Map<String, dynamic> json) {
     id = json['id'];
     authorName = json['author_name'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
     reactionType = json['reaction_type'];
     author = json['author'];
     content = json['content'];
@@ -20,6 +30,8 @@ class Reaction {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['id'] = this.id;
     data['author_name'] = this.authorName;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
     data['reaction_type'] = this.reactionType;
     data['author'] = this.author;
     data['content'] = this.content;
